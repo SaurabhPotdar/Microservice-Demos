1c9237514c506dd676d94707187143818b85ce86
Saurabh Potdar <44492033+SaurabhPotdar@users.noreply.github.com>
2020-09-30 17:59:55 +0530

    Spring Config Server Demo

---
+++ SpringCloudConfigDemo/Config-Client/src/main/java/com/cg/configclient/Controller.java
@@ -0,0 +1,18 @@
+package com.cg.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@Value("${app.property}")
+	private String message;
+	
+	@RequestMapping
+	public String getProperty() {
+		return message;
+	}
+	
+}
