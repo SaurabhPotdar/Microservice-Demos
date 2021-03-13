2dde67f0cf9c72e42ad37ce885451cf7b73f0538
Saurabh Potdar <44492033+SaurabhPotdar@users.noreply.github.com>
2021-03-13 21:18:38 +0530

    Update FeignProxy.java

--- CircuitBreakerDemo/ProductServiceCatalog/src/main/java/com/cg/productservicecatalog/controller/FeignProxy.java
+++ CircuitBreakerDemo/ProductServiceCatalog/src/main/java/com/cg/productservicecatalog/controller/FeignProxy.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.cg.productservicecatalog.dto.Product;
 
+//Name of external service we want to call
 @FeignClient(name="product-service", fallback = FeignProxyFallback.class)
 public interface FeignProxy {
 	
+	//url of external service
 	@GetMapping("/products/get")
 	public List<Product> get();
 
